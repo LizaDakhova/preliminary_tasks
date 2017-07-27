913801138df0285dd7840b0b3520964d21699bbc
elize <donskaya0598@gmail.com>
2017-07-27 19:50:07 +0300

    It is better than before

--- task.cpp
+++ task.cpp
@@ -1,20 +1,25 @@
 #include <iostream>
 #include <stdlib.h>
-#include <string>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
 	if (argc == 1)
 		std::cerr << "Type a natural number.\n";
 	else {
-		int n = atoi(argv[1]);
-		std::cout << n << '\n';
-		if (n == 0 && *argv[1] != '0')
-			std::cerr << "Use only digits to type the natural number.\n";
-		else if (n < 0)
-			std::cerr << "The negative number is unacceptable.\n";
-		else
+		int index = 0;
+		bool cont = true;
+		for(unsigned int i = 0; i < strlen(argv[1]) && cont; ++i) {
+			if (argv[1][i] < '0' || argv[1][i] > '9') {
+				std::cerr << "Use only digits to type the natural number.\n";
+				cont = false;
+				++index;
+			}
+		}
+		if (cont) {
+			int n = atoi(argv[1]);
 			for (int i = 1; i <= n; ++i)
 				std::cout << i << '\n';
+		}
 	}
 	return 0;
 }
